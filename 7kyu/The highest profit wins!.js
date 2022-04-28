ccf0842a2f0d3e700e79cb67bb47df34f1439a36
Christopher Aaron <christops.codes@gmail.com>
2022-04-28 12:56:16 -0700

    add new challenge and solution

---
+++ 7kyu/The highest profit wins!.js
@@ -0,0 +1,18 @@
+// // Story
+// // Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+// // Task
+// // Write a function that returns both the minimum and maximum number of the given list/array.
+
+// // Remarks
+// // All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+// Remarks
+// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+function minMax(arr) {
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+
+    return [min, max];
+}
