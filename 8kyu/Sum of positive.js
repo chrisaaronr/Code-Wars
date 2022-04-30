40260b6f18208349bd112cf428e2dc26190c07e6
Christopher Aaron <christops.codes@gmail.com>
2022-04-30 14:42:40 -0700

    create 'sum of positive' kata and solution

---
+++ 8kyu/Sum of positive.js
@@ -0,0 +1,27 @@
+// SUM OF POSITIVE
+
+//You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// // SOLUTION
+
+// P: arr, num, positive, whole
+// R: return, sum of numbers
+// E: [1, 2, 3] -> 6; [4, 5, 6] --> 15
+// P:
+function positiveSum(arr) {
+    let total = 0;    
+    for (i = 0; i < arr.length; i++) {    
+    // setup loop to go through array of given length
+      if (arr[i] > 0) {                   
+      // if arr[i] is greater than zero
+        total += arr[i];                  
+       // add arr[i] to total
+      }
+    }
+    return total;                        
+    // return total
+  }
