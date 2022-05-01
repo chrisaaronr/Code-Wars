a3dc20b00a6cb1e11b60e455669e3c1a6da3a3f5
Christopher Aaron <christops.codes@gmail.com>
2022-05-01 16:33:39 -0700

    create 'Are You Playing Banjo' problem and solution

---
+++ 8kyu/Are you playing banjo?.js
@@ -0,0 +1,16 @@
+// Are You Playing Banjo?
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// EXAMPLE
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+//SOLUTION
+function areYouPlayingBanjo(name) {
+    if (name[0] === 'R' || name[0] === 'r') return `${name} plays banjo`
+    return `${name} does not play banjo`
+  }
