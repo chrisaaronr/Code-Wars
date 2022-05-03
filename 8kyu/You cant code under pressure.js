1b510716e49d80c49dd0759bf10fb5c0a6abc499
Christopher Aaron <christops.codes@gmail.com>
2022-05-03 14:46:31 -0700

    create 'you cant code under pressure' challenge and solution

---
+++ 8kyu/You cant code under pressure.js
@@ -0,0 +1,8 @@
+// // You Cant Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// // SOLUTION
+function doubleInteger(i) {
+    return i + i;
+  }
