374e0c225d321dae87ae45b33e1243da9a7433c8
Christopher Aaron <christops.codes@gmail.com>
2022-05-08 14:06:10 -0700

    edit include PREP into solution

--- 7kyu/Odd or Even?.js
+++ 7kyu/Odd or Even?.js
@@ -7,15 +7,6 @@ Give your answer as a string matching "odd" or "even".
 
 If the input array is empty consider it as: [0] (array with a zero).
 
-Examples:
-Input: [0]
-Output: "even"
-
-Input: [0, 1, 4]
-Output: "odd"
-
-Input: [0, -1, -5]
-Output: "even"
 */
 
 //SOLUTION
