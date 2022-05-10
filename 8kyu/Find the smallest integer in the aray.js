82648d71157851f06d880009b5b987e687201570
Christopher Aaron <christops.codes@gmail.com>
2022-05-09 21:48:53 -0700

    create 'find the smallest integer in the array' kata and solution

---
+++ 8kyu/Find the smallest integer in the aray.js
@@ -0,0 +1,21 @@
+// FIND THE SMALLEST INTEGER IN THE ARRAY
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+
+// SOLUTION 
+function findSmallestInt(args) {
+    let smallest = args[0]
+
+    for(let i = 0; i < args.length; i++){
+        if(args[i] < smallest) smallest = args[i]
+    }
+
+    return smallest
+}
