c944c3cd9dc8441ee5780cbff534b7446cd99096
Christopher Aaron <christops.codes@gmail.com>
2022-05-11 17:04:36 -0700

    add kata 'expression matters' and solution

---
+++ 8kyu/Expression Matters.js
@@ -0,0 +1,9 @@
+// // EXPRESSION MATTERS
+// Task
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+
+//SOLUTION
+function expressionMatter(a, b, c) {
+    return Math.max(a + b + c, a * b * c, (a + b) * c, a * (b + c))
+  }
