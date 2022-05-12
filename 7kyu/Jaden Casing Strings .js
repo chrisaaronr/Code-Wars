9f4171f84ce2352d1363e5f0eefb7f3f44f68402
Christopher Aaron <christops.codes@gmail.com>
2022-05-12 16:27:47 -0700

    create 'Jaden Casing Strings' kata and solution

---
+++ 7kyu/Jaden Casing Strings .js
@@ -0,0 +1,13 @@
+// // JADEN CASING STRINGS
+
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// SOLUTION
+
+String.prototype.toJadenCase = function() {
+    return this.split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+  }
