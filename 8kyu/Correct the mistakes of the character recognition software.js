ed7e8ca4b19b8fab911ee3ed084eb99160f46889
Christopher Aaron <christops.codes@gmail.com>
2022-06-05 12:36:50 -0700

    create 'correct the mistakes...software' kata and solution

---
+++ 8kyu/Correct the mistakes of the character recognition software.js
@@ -0,0 +1,25 @@
+// CORRECT THE MISTAKES OF THE CHARCTER RECOGNITION SOFTWARE
+
+/*
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+*/
+
+
+// SOLUTION
+const correct = string =>
+  string
+    .split('')
+    .map(char => {
+      const corrections = { '0': 'O', '5': 'S', '1': 'I' }
+      return corrections[char] || char
+    })
+    .join('')
