06b815b366d93f611a6e12949cb37b6645fc1bbd
Christopher Aaron <christops.codes@gmail.com>
2023-05-15 16:55:31 -0700

    add 'Length and two values' kata

---
+++ 7kyu/Length and two values.js
@@ -0,0 +1,24 @@
+// Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+// SOLUTION
+
+function alternate(n, firstValue, secondValue) {
+    let arr = [];
+
+    for (i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            arr.push(firstValue);
+        } else {
+            arr.push(secondValue)
+        }
+    }
+    return arr
+}
+
+// Alt. Solutions
+function alternate(n, firstValue, secondValue){
+    const array = [];
+    for (let i = 0; i < n; i++) {
+      array.push(i % 2 === 0 ? firstValue : secondValue)
+    }
+    return array;
--- 8kyu/Find the smallest integer in the aray.js
+++ 8kyu/Find the smallest integer in the aray.js
@@ -1,12 +1,12 @@
 // FIND THE SMALLEST INTEGER IN THE ARRAY
 
-Given an array of integers your solution should find the smallest integer.
+// Given an array of integers your solution should find the smallest integer.
 
-For example:
+// For example:
 
-Given [34, 15, 88, 2] your solution will return 2
-Given [34, -345, -1, 100] your solution will return -345
-You can assume, for the purpose of this kata, that the supplied array will not be empty.
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
 
 // SOLUTION 
