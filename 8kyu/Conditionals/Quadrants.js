88e586262b59bcf026745d24928a1de99997e338
Christopher Aaron <christops.codes@gmail.com>
2023-06-01 12:34:17 -0700

    add Quadrants kata

---
+++ 8kyu/Conditionals/Quadrants.js
@@ -0,0 +1,31 @@
+// Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+/* There are four quadrants:
+
+First quadrant, the quadrant in the top-right, contains all points with positive X and Y
+Second quadrant, the quadrant in the top-left, contains all points with negative X, but positive Y
+Third quadrant, the quadrant in the bottom-left, contains all points with negative X and Y
+Fourth quadrant, the quadrant in the bottom-right, contains all points with positive X, but negative Y
+*/
+
+// Solution
+
+function quadrant(x, y) {
+    if (x > 0 && y > 0) {
+        return 1;
+    } else if (x < 0 && y > 0) {
+        return 2;
+    } else if (x < 0 && y < 0) {
+        return 3;
+    } else {
+        return 4;
+    }
+};
+
+
+function quadrant(x, y) {
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    return 4;
+  }
